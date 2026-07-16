65ec2775bc5b3f491b3ee308979d66d36c921e6b
umarKhan <mromertariq@gmail.com>
2026-07-16 15:28:47 +0500

    function overloading

--- Oops/polymorphism.cpp
+++ Oops/polymorphism.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
-
+class Print {
+  public: 
+  void print(int x){
+    cout<<"Int "<<x<<endl;
+  }
+  void print(char ch ){
+    cout<<"char "<<ch<<endl;
+  }
+};
 
 int main (){ ;
-  
+  Print p1;
+  p1.print('a'); 
   return 0;
 }
