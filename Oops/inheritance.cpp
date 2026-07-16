ad98cdf967c619f119940e42361c7914425a4287
umarKhan <mromertariq@gmail.com>
2026-07-16 15:11:49 +0500

    include parameterized constructors in inheritance

--- Oops/inheritance.cpp
+++ Oops/inheritance.cpp
@@ -9,6 +9,12 @@ class Person {
   Person(){
     cout<<"this is Person class constructor"<<endl;
   }
+  Person(string name , int age){
+    this->name = name;
+    this->age = age;
+    
+  }
+
   ~Person(){
     cout<<"Person destructor\n";
   }
@@ -22,17 +28,23 @@ class Student: public Person {
       cout<<"this is student class constructor"<<endl; // parent consstructor is called first
     }
 
+    Student(string name, int age,int rollNo):Person(name, age){
+      this->rollNo = rollNo;
+      cout<<"child constructor\n"<<endl;
+    }
+    void displayInfo(){
+      cout<<"name: "<<name<<endl;
+      cout<<"age: "<<age<<endl;
+      cout<<"roll no: "<<rollNo<<endl;
+    }
+
     ~Student(){
       cout<<"Student Destructor\n"; // Child Destructor is called first
     }
     
 };
 int main (){ ;
-  Student s1;
-  s1.name = "Umar Khan";
-  s1.age = 22;
-  s1.rollNo = 231184;
-  cout<< s1.name<<endl;
-  cout<< s1.rollNo<<endl;
+  Student s1("umar Khan", 21,231184);
+  s1.displayInfo();
   return 0;
 }
