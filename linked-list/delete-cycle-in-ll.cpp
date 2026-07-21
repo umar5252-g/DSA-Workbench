99980b50b5168c2a5dd2c9552bd186c5965b7aa2
umarKhan <mromertariq@gmail.com>
2026-07-21 10:50:29 +0500

    fix error

--- linked-list/delete-cycle-in-ll.cpp
+++ linked-list/delete-cycle-in-ll.cpp
@@ -1,6 +1,89 @@
 #include<iostream>
 using namespace std;
-#include<list>
+
+
+class Node {
+  public:
+  int data;
+  Node* next;
+
+  Node(int val){
+    data = val;
+    next = NULL;
+  }
+
+};
+
+class List {
+  private: 
+  Node* head ;
+  Node* tail ;
+
+  public:
+  List(){
+    head = tail = NULL;
+  }
+  void push_front(int val){
+    Node* newNode = new Node(val);
+
+    if(head == NULL){
+      head = tail = newNode;
+    }
+    else{
+      newNode->next=head;
+      head = newNode;
+    }
+  }
+
+  void push_back(int val){
+    Node* newNode = new Node(val);
+
+    if(head == NULL){
+      head = tail = newNode;
+    }
+    else{
+     tail->next = newNode;
+     tail = newNode;
+    }
+  }
+
+  void pop_front(){
+    if(head == NULL){
+      cout<<"LL is empty\n";
+      return;
+    }
+    Node* temp = head;
+    head = head->next;
+    temp->next = NULL;
+
+    delete temp;
+  }
+
+  void pop_back(){
+    if(head == NULL){
+      cout<<"LL is empty\n";
+      return;
+    }
+    Node* temp = head;
+    while(temp->next != tail){
+      temp = temp->next;
+    }
+    temp->next = NULL;
+    delete temp;
+    tail = temp;
+   
+    
+  }
+  void print_all(){
+    Node* temp = head;
+    while(temp !=  NULL){
+      cout<<temp->data<<"->";
+      temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+  }
+
+};
 
 bool deleteCycle(Node* head){
     
