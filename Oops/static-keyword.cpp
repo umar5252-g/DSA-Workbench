ff1beb8551800dbda13b1020e33dd29bd65d1cce
umarKhan <mromertariq@gmail.com>
2026-07-21 08:31:58 +0500

    simple variable in class

--- Oops/static-keyword.cpp
+++ Oops/static-keyword.cpp
@@ -1,15 +1,29 @@
 #include<iostream>
 using namespace std;
 
-void fun(){
-  static int x = 0 ;
-  cout<<"x: "<<x<<endl;
-  x++;
-}
+class A{
+   public:
+   int x ; 
+   void intX(){
+    x = x+1;
+   }
+};
+// void fun(){
+//   static int x = 0 ;
+//   cout<<"x: "<<x<<endl;
+//   x++;
+// }
 
 int main (){ ;
-  fun();
-  fun();
-  fun();
+  A obj1;
+  A obj2;
+
+  obj1.x = 100;
+  obj2.x = 200;
+
+  cout<<obj1.x<<endl;
+  cout<<obj2.x<<endl;
+
+
   return 0;
 }
