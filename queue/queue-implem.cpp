549d6726ba2f96141475cb5a6f472aba46e56706
umarKhan <mromertariq@gmail.com>
2026-08-20 13:30:53 +0500

    implement testing of queue methods

--- queue/queue-implem.cpp
+++ queue/queue-implem.cpp
@@ -71,7 +71,17 @@ public:
 
 int main()
 {
-  ;
+  queue q;
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  q.push(4);
+  while (!q.empty())
+  {
+    cout << q.front() << " ";
+    q.pop();
+  }
+  cout << endl;
 
   return 0;
 }
